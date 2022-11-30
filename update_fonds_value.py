e9417347984cb62ed0b3f4dea2c9ff5c462f6070
ElonRM <t-ehmann@gmx.de>
2022-11-30 14:00:36 +0100

    pylint clean

--- update_fonds_value.py
+++ update_fonds_value.py
@@ -1,5 +1,6 @@
-import pandas as pd
+"""Updater for the value of each position and the complete Fund"""
 import datetime
+import pandas as pd
 
 inventory = pd.read_csv('Fonds_Positions_Test.csv', index_col=0)
 
@@ -10,6 +11,7 @@ absolute_invested = round((inventory.Position_purchase_price*inventory.Quantity)
 gain_relative = round(absolute_value/absolute_invested*100-100,2)
 gain_absolute = absolute_value-absolute_invested
 
-with open(r'value_history.csv', 'a') as fonds_value_history:
-    fonds_value_history.write(f"\n{timestamp},{absolute_invested},{absolute_value},{gain_relative},{gain_relative}")
-
+with open(r'value_history.csv', 'a', encoding='UTF-8') as fonds_value_history:
+    fonds_value_history.write(
+        f"\n{timestamp},{absolute_invested},{absolute_value},{gain_relative},{gain_relative}"
+        )
