a0be47285005075ea6d598fd3b62e3c28180bcb4
ElonRM <t-ehmann@gmx.de>
2022-12-07 01:20:54 +0100

    update und test

--- add_new_purchase.py
+++ add_new_purchase.py
@@ -1,13 +1,9 @@
 """Adding new Purchases to the Fund using a csv file"""
 
 import pandas as pd
+import datetime
+import fund_variables as fv
 
-#FILENAME = 'Fund_Positions_Test.csv'
-FILENAME = 'Fund_Positions.csv'
-LIQUID_FUNDS = 'Liquid_Funds.csv'
-
-inventory = pd.read_csv(FILENAME, index_col=0)
-purchase_data = pd.read_csv('new_purchases.csv')
 
 def reset_all_positions():
     """Sets all Position_Sizes to 0"""
@@ -32,16 +28,33 @@ def add_buy_position(item_id, quantity, price):
     inventory.loc[inventory.Item_ID == item_id, 'Gain_relative'] = new_gain_relative
     inventory.loc[inventory.Item_ID == item_id, 'Gain_absolute'] = new_gain_absolute
 
-#reset_all_positions()
+inventory = pd.read_csv(fv.FUND_FILENAME, index_col=0)
+liquid_funds = fv.LIQUID_FUNDS
+#liquid_funds = pd.read_csv(fv.LIQUID_FUNDS, index_col=0).iloc[0]['Liquid_Funds']
+purchase_data = pd.read_csv(fv.PURCHASE_DATA)
+order_history = pd.read_csv(fv.ORDER_HISTORY, index_col=0)
 
+reset_all_positions()
+
+# Updates the Fund Values for each bought Item
 for index, row in purchase_data.iterrows():
     add_buy_position(row['Item_ID'], row['Quantity'], row['Purchase_Price'])
     #pass
 
-# Update new percentages of the Portfolio
-liquid_funds = pd.read_csv(LIQUID_FUNDS, index_col=0).iloc[0]['Liquid_Funds']
+# Adding the purchase to the purchase history
+total_order_price = round(sum(purchase_data.Quantity*purchase_data.Purchase_Price),2)
+order_history = order_history.append({'Date': datetime.datetime.now().date(),
+                                        'Item_IDs': purchase_data.Item_ID.to_list(),
+                                        'Prices': purchase_data.Purchase_Price.to_list(),
+                                        'Quantities': purchase_data.Quantity.to_list(),
+                                        'Total_Order_Price': total_order_price},
+                                    ignore_index = True
+                                    )
+
+# Updating the percentages of the Portfolio
 fund_value = inventory['Position_Value'].sum()
 print(fund_value, liquid_funds)
 inventory.Percentage = round(inventory.Position_Value/(fund_value+liquid_funds)*100,2)
 
-inventory.to_csv(FILENAME)
+inventory.to_csv(fv.FUND_FILENAME)
+order_history.to_csv(fv.ORDER_HISTORY)
---
+++ fund_variables.py
@@ -0,0 +1,9 @@
+''' Includes all the globally used variables'''
+
+
+FUND_FILENAME = 'Fund_Positions.csv'
+ORDER_HISTORY = 'buy_order_history.csv'
+PURCHASE_DATA = 'new_purchases.csv'
+
+LIQUID_FUNDS = 0
+AMOUNT_INVESTED = 0
