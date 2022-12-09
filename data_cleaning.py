3097dd7a0e8eb5daa57a90f5f9c5df1dd90fc70a
ElonRM <t-ehmann@gmx.de>
2022-12-09 02:03:03 +0100

    fund value and percentages still not fixed

--- data_cleaning.py
+++ data_cleaning.py
@@ -21,7 +21,7 @@ df = pd.read_csv(fv.FUND_FILENAME, index_col=0)
 #print(df.columns)
 #print(df.dtypes)
 
-#update_fund_value.save_fund_value(df, liquid_funds = pd.read_csv(fv.DYNAMIC_VARIABLES).iloc[0]['Liquid_Funds'])
+update_fund_value.save_fund_value(df, liquid_funds = pd.read_csv(fv.DYNAMIC_VARIABLES).iloc[0]['Liquid_Funds'])
 # dv = pd.read_csv(fv.DYNAMIC_VARIABLES)
 # print(dv.loc[0, 'Liquid_Funds'])
 # order_price = 1000
@@ -30,19 +30,20 @@ df = pd.read_csv(fv.FUND_FILENAME, index_col=0)
 # liquid_funds = dv.loc[0,'Liquid_Funds']
 # print(liquid_funds)
 
-buy_order_history = pd.read_csv(fv.BUY_ORDER_HISTORY, index_col=0)
-purchase_data = pd.read_csv(fv.PURCHASE_DATA)
+# buy_order_history = pd.read_csv(fv.BUY_ORDER_HISTORY, index_col=0)
+# purchase_data = pd.read_csv(fv.PURCHASE_DATA)
 
-order_price = 2000
+# order_price = 2000
 
-import datetime
-buy_order_history = pd.concat([buy_order_history,
-                                pd.DataFrame({'Date': datetime.datetime.now().date(),
-                                            'Item_IDs': purchase_data.Item_ID.to_list(),
-                                            'Prices': purchase_data.Purchase_Price.to_list(),
-                                            'Quantities': purchase_data.Quantity.to_list(),
-                                            'Total_Order_Price': order_price})],
-                                ignore_index=True
-                                )
-print(purchase_data.Quantity.to_list())
-print(buy_order_history)
+# import datetime
+# buy_order_history = pd.concat([buy_order_history,
+#                                 pd.DataFrame({'Date': datetime.datetime.now().date(),
+#                                             'Item_IDs': purchase_data.Item_ID.to_list(),
+#                                             'Prices': purchase_data.Purchase_Price.to_list(),
+#                                             'Quantities': purchase_data.Quantity.to_list(),
+#                                             'Total_Order_Price': order_price})],
+#                                 ignore_index=True
+#                                 )
+# print(purchase_data.Quantity.to_list())
+# print(buy_order_history)
+# #buy_order_history.to_csv(fv.BUY_ORDER_HISTORY)
